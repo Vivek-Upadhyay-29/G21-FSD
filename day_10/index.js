cc099ee9676bfe385376f306b309730be459cdda
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-07-25 22:24:51 +0530

    commit

--- day_10/index.js
+++ day_10/index.js
@@ -29,27 +29,34 @@
 // let multi = (a,b)=> a*b;
 // console.log(multi(8,7))
 
-var day = Math.floor(Math.random() * 7);
-switch (day) {
-  case 1:
-    console.log("the given number is 1 and day is Sunday");
-    break;
-  case 2:
-    console.log("the given number is 2 and day is Monday");
-    break;
-  case 3:
-    console.log("the given number is 3 and day is Tuesday");
-    break;
-  case 4:
-    console.log("the given number is 4 and day is Wednesday");
-    break;
-  case 5:
-    console.log("the given number is 5 and day is Thursday");
-    break;
-  case 6:
-    console.log("the given number is 6 and day is Friday");
-    break;
-  case 7:
-    console.log("the given number is 7 and day is Saturday");
-    break;
+// var day = Math.floor(Math.random() * 7);
+// switch (day) {
+//   case 1:
+//     console.log("the given number is 1 and day is Sunday");
+//     break;
+//   case 2:
+//     console.log("the given number is 2 and day is Monday");
+//     break;
+//   case 3:
+//     console.log("the given number is 3 and day is Tuesday");
+//     break;
+//   case 4:
+//     console.log("the given number is 4 and day is Wednesday");
+//     break;
+//   case 5:
+//     console.log("the given number is 5 and day is Thursday");
+//     break;
+//   case 6:
+//     console.log("the given number is 6 and day is Friday");
+//     break;
+//   case 7:
+//     console.log("the given number is 7 and day is Saturday");
+//     break;
+// }
+
+
+generateRandom(a,b){
+ 
+    var no = Math.floor(Math.random() * 11);
+
 }
---
+++ day_11/index.js
@@ -0,0 +1,3 @@
+document.getElementById("btnn").onclick=function(){
+    document.getElementById("sample").style.color= 'red';
+};
